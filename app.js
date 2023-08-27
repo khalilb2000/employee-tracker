347b91c7ee094f808862988762ec331a0e42dd7e
Khalil Barry <khalilbarry@Khalils-MBP.attlocal.net>
2023-08-27 11:40:33 -0500

    Adding a try catch for my viewAllRoles function

--- app.js
+++ app.js
@@ -29,5 +29,31 @@ switch (answer.choice){
                case 'Add a role':
                 viewAllRoles();
                 break;
+
+                case 'Exit':
+                    console.log('Goodbye!');
+                    break; 
                 
+}
+
+async function viewAllDepartments(){
+    try{
+        const [departments] = await Queries.getAllDepartments();
+        console.table(departments);
+        mainMenu();
+
+    } catch (error){
+        console.error('Error fetching departments:', error);
+        mainMenu();
+    }
+}
+
+async function viewAllRoles(){
+try {
+    const [roles] = await Queries.viewAllRoles
+} catch (error) {
+    console.error('Error fetching all roles:', error);
+    mainMenu()
+}
+
 }
