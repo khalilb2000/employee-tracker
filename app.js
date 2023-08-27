2dae275317457a5f903b89bd7701c5ce217d4bda
Khalil Barry <khalilbarry@Khalils-MBP.attlocal.net>
2023-08-27 10:12:48 -0500

    intergrating switch function

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const inquirer = require('inquirer');
 const Queries = require('./queries');
 
+// the statrt of function
 async function mainMenu(){
+// User action prompt
     const answer = await inquirer.createPromptModule([
         {
             type: 'list',
@@ -16,4 +18,16 @@ async function mainMenu(){
             ]
         }
     ])
+}
+
+switch (answer.choice){
+// Switching prompts
+    case 'View all departments':
+            viewAllDepartments();
+            break;   
+            
+               case 'Add a role':
+                viewAllRoles();
+                break;
+                
 }
