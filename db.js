04a683083683615c1b69c25f5301299f9b7df071
Khalil Barry <khalilbarry@Khalils-MBP.attlocal.net>
2023-08-28 17:10:10 -0500

    Fixed a couple of issues with my app.js as well as re-installed my packages.

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user:'root',
     password:'',
-    database:'your_database',
+    database:'employee_db',
 })
 
 module.exports = connection;
--- queries.js
+++ queries.js
@@ -1,11 +1,27 @@
-const connection = require('db.js');
+const connection = require('./db');
 
 class Queries {
     static getAllDepartments(){
         return connection.promise().query('SELECT * FROM department');
+    }
 
-        
+    static getAllRoles() {
+        return connection.promise().query('SELECT * FROM role');
+    }
+
+    static addRole(title,salary,departmentId){
+        // constructing and executing mySQL query
+        const sql = 'INSERT INTO role (title, salary, department_id) VALUES (?,?,?)';
+        return connection.promise().query(sql,[title,salary,departmentId]);
+    }
+
+    static addDepartment(name){
+        const sql = 'INSERT INTO department (name) VALUES (?)'
+        return connection.promise().query(sql,[name]);
     }
 }
+
+
+
 module.exports = Queries;
 
