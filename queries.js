a55e8f016d4d9c9d6696fb9cf2338cf715a2e775
khalilb2000 <khalilb2000@gmail.com>
2023-11-14 09:37:11 -0600

    updating queries and connection

--- queries.js
+++ queries.js
@@ -1,32 +1,45 @@
-// funcion for executing queries ( don't forget to return the promise)
-const connection = require('./db')
+const connection = require('./db');
 
 class Queries {
     
     static getAllDepartments() {
-        return connection.promise().query('SELECT * FROM department');
+        return connection.promise().query('SELECT * FROM department')
+            .catch((error) => {
+                throw new Error(`Error in getAllDepartments: ${error.message}`);
+            });
     }
 
     static getAllRoles() {
-        return connection.promise().query('SELECT * FROM role');
+        return connection.promise().query('SELECT * FROM role')
+            .catch((error) => {
+                throw new Error(`Error in getAllRoles: ${error.message}`);
+            });
     }
 
     static addRole(title, salary, departmentId) {
-        // constructing and executing mySQL query
         const sql = 'INSERT INTO role (title, salary, department_id) VALUES (?,?,?)';
-        return connection.promise().query(sql, [title, salary, departmentId]);
+        console.log('Executing query:', sql);
+        return connection.promise().query(sql, [title, salary, departmentId])
+            .catch((error) => {
+                throw new Error(`Error in addRole: ${error.message}`);
+            });
     }
 
     static addDepartment(name) {
         const sql = 'INSERT INTO department (name) VALUES (?)';
-        return connection.promise().query(sql, [name]);
+        console.log('Executing query:', sql);
+        return connection.promise().query(sql, [name])
+            .catch((error) => {
+                throw new Error(`Error in addDepartment: ${error.message}`);
+            });
     }
 
-    static quit(){
-        return connection.promise().end();
+    static quit() {
+        return connection.promise().end()
+            .catch((error) => {
+                throw new Error(`Error in quit: ${error.message}`);
+            });
     }
 }
-
-
 
 module.exports = Queries;
